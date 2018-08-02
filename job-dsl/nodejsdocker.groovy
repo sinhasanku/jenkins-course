5ed5ea176aeb72a5b4d9cc747cb5ac6113862c8d
sinhasanku <30862279+sinhasanku@users.noreply.github.com>
2018-08-02 15:39:20 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/sinhasanku/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('sanku@sparce.co.uk')
         }
     }
     triggers {
